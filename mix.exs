15bf58566b7efdbce8d4101de3e4ac7a35448d25
Blain Smith <rebelgeek@blainsmith.com>
2017-03-28 16:00:56 -0400

    add Poison

--- mix.exs
+++ mix.exs
@@ -28,6 +28,9 @@ defmodule RokuIapElixir.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.10.0"}]
+    [
+      {:httpoison, "~> 0.10.0"},
+      {:poison, "~> 3.1.0"}
+    ]
   end
 end
