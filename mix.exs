0b80d078d562bcfa35bd6801b303f71f810cb4a8
Erik Straub <erik.straub@oddnetworks.com>
2017-07-26 11:57:43 -0400

    Updates mix.exs for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -2,31 +2,21 @@ defmodule RokuIAP.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :roku_iap_elixir,
+    [app: :roku_iap,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
+     description: description(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [applications: [:httpoison], extra_applications: [:logger]]
+    [applications: [:httpoison],
+     extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.11.1"},
@@ -34,5 +24,19 @@ defmodule RokuIAP.Mixfile do
       {:hackney, "~> 1.7.1"},
       {:meck, "~> 0.8.2", only: :test}
     ]
+  end
+
+  defp description do
+    """
+    An Elixir HTTP client for interacting with the Roku IAP REST API
+    """
+  end
+
+  defp package do
+    [name: :roku_iap,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Blain Smith", "Erik Straub"],
+     licenses: ["Apache 2.0"],
+     links: %{GitHub: "https://github.com/oddnetworks/roku_iap_elixir"}]
   end
 end
