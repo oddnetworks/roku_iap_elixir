7afb8d918eb673875ca946b0a11db8fa777407a0
Erik Straub <erik.straub@oddnetworks.com>
2017-07-26 15:00:28 -0400

    Bumps version 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RokuIAP.Mixfile do
 
   def project do
     [app: :roku_iap,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
